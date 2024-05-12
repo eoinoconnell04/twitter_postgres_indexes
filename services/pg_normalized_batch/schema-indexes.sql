77278889b2fea78cd5dfb6d820f48d6b8f881e72
eoinoconnell04 <156839596+eoinoconnell04@users.noreply.github.com>
2024-05-12 09:36:25 -0700

    Update schema-indexes.sql

--- services/pg_normalized_batch/schema-indexes.sql
+++ services/pg_normalized_batch/schema-indexes.sql
@@ -3,3 +3,4 @@ CREATE INDEX CONCURRENTLY two ON tweet_tags(id_tweets, tag);
 CREATE INDEX CONCURRENTLY three ON tweets(id_tweets, lang);
 CREATE INDEX CONCURRENTLY four ON tweets(lang);
 CREATE INDEX CONCURRENTLY five ON tweets using gin(to_tsvector('english',text));
+-- Only inlude concurrently if you are building the indexes simulataneously to loading the data. Otherwise it slows the building of the indexes by a factor of 2.
